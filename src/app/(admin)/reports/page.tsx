b4a22eb74c937d380759e8695a7a2e3139f3ccb2
fuadhossainnishad <fuadhossainbk01@gmail.com>
2025-09-16 00:03:40 +0600

    update dashboard

--- src/app/(admin)/reports/page.tsx
+++ src/app/(admin)/reports/page.tsx
@@ -1,19 +1,17 @@
 "use client";
-import React, { useState } from "react";
+import React from "react";
 import StoriesTable from "./_components/StoriesTable";
 import UserGrowthChart from "./_components/charts/Charts";
-import YearPicker from "./_components/YearPicker"; // Import YearPicker component
+// import YearPicker from "./_components/YearPicker"; // Import YearPicker component
 
 export default function ReportsPage() {
   // Set the default selected year to the current year
-  const [selectedYear, setSelectedYear] = useState<string>(
-    new Date().getFullYear().toString()
-  );
+  const selectedYear = new Date().getFullYear().toString();
 
   // Handle year change
-  const handleYearChange = (year: string) => {
-    setSelectedYear(year);
-  };
+  // const handleYearChange = (year: string) => {
+  //   setSelectedYear(year);
+  // };
 
   return (
     <main className="space-y-10">
@@ -30,10 +28,10 @@ export default function ReportsPage() {
             <h1 className="text-[#111827] font-semibold text-lg">
               User Growth Over Time
             </h1>
-            <YearPicker
+            {/* <YearPicker
               selectedYear={selectedYear}
               onYearChange={handleYearChange}
-            />
+            /> */}
           </div>
 
           <UserGrowthChart selectedYear={selectedYear} />
@@ -43,10 +41,10 @@ export default function ReportsPage() {
             <h1 className="text-[#111827] font-semibold text-lg">
               Stories Created by Age Group
             </h1>
-            <YearPicker
+            {/* <YearPicker
               selectedYear={selectedYear}
               onYearChange={handleYearChange}
-            />
+            /> */}
           </div>
 
           <UserGrowthChart selectedYear={selectedYear} />
